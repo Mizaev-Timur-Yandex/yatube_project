51a9a3881b36426742ef1d0d2aa9d18086c49631
Тимур Мизаев <temur.mizaev@yandex.ru>
2022-11-19 03:10:58 +0300

    Добавлены namespace, css, img

--- yatube/posts/urls.py
+++ yatube/posts/urls.py
@@ -2,8 +2,11 @@ from django.urls import path
 
 from . import views
 
+app_name = 'posts'
+
 urlpatterns = [
     path('', views.index),
+    path('posts/', views.index, name='posts_index'),
     path('group/<slug:slug>/', views.group_posts),
-    path('group_list/<slug:slug>/', views.group_list),
+    path('group_list.html', views.group_list),
 ]
--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -3,11 +3,25 @@ from django.shortcuts import render
 
 def index(request):
     template = 'posts/index.html'
-    return render(request, template)
+    title = 'Yatube для друзей'
+    text = 'Это главная страница проекта Yatube'
+    context = {
+        'text': text,
+    }
+    return render(request, template, context)
 
 def group_posts(request, slug):
-    return HttpResponse(f'Страница группы: {slug}')
+    template = 'posts/index.html'
+
+    return HttpResponse(request,f'Страница группы: {slug}')
 
 def group_list(request):
     template = 'posts/group_list.html'
-    return render(request, template)
+    title = 'Yatube для друзей'
+    text = 'Здесь будет информация о группах проекта Yatube'
+    context = {
+
+        'title': title,
+        'text': text,
+    }
+    return render(request, template, context)
