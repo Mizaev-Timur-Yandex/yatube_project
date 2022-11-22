028fe9af0d1f71a593466079091f55c1359ad7ef
Тимур Мизаев <temur.mizaev@yandex.ru>
2022-11-22 04:57:08 +0300

    Выполнена миграция

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,14 +1,17 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Post
 
 def index(request):
-    template = 'posts/index.html'
-    title = 'Yatube для друзей'
-    text = 'Это главная страница проекта Yatube'
+    # Одна строка вместо тысячи слов на SQL:
+    # в переменную posts будет сохранена выборка из 10 объектов модели Post,
+    # отсортированных по полю pub_date по убыванию (от больших значений к меньшим)
+    posts = Post.objects.order_by('-pub_date')[:10]
+    # В словаре context отправляем информацию в шаблон
     context = {
-        'text': text,
+        'posts': posts,
     }
-    return render(request, template, context)
+    return render(request, 'posts/index.html', context)
 
 def group_posts(request, slug):
     template = 'posts/group_posts.html'
